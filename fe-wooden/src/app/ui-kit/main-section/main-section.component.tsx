dd44619445aee1b85c882a9a0572c73060de3772
Gordeev Dmitry <jadima2015@gmail.com>
2023-10-28 15:31:35 +0300

    [FE] [FE-2] Section main page// Corrected classies nesting

--- fe-wooden/src/app/ui-kit/main-section/main-section.component.tsx
+++ fe-wooden/src/app/ui-kit/main-section/main-section.component.tsx
@@ -5,21 +5,21 @@ import css from './main-section.module.css';
 export const MainSection = () => {
     return (
         <div className={css.wrap}>
-            <div className={css.infoBar}>
-                <span className={css.infoTitle}>
+            <div className={css.info}>
+                <span className={css.title}>
                     Crafted Wooden <br />
                     Games from England
                 </span>
-                <span className={css.infoText}>
+                <span className={css.text}>
                     Many people are familiar with this game,but at this size
                     it's a very different experience.
                 </span>
             </div>
 
-            <div className={css.linksBar}>
-                <div className={css.linksElement}>Games for Team Building</div>
-                <div className={css.linksElement}>Play Area for the Party </div>
-                <div className={css.linksElement}>
+            <div className={css.links}>
+                <div className={css.element}>Games for Team Building</div>
+                <div className={css.element}>Play Area for the Party </div>
+                <div className={css.element}>
                     Welcome Area for the Wedding
                 </div>
             </div>
