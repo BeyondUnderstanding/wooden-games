3964fb57b7157bdf4742eb476464c3df87971a9d
Litinsci <litovchenko.73@list.ru>
2023-11-01 20:39:25 +0400

    MainSection

--- fe-wooden/src/app/page.tsx
+++ fe-wooden/src/app/page.tsx
@@ -5,6 +5,7 @@ import { Products } from './ui-kit/products/products.component';
 // MOCK
 import { productsBasket } from './ui-kit/side-popup/popup.mock';
 import src from '../app/ui-kit/busket-product-card/images/zigzag.png';
+import { MainSection } from './ui-kit/main-section/main-section.component';
 
 export default function Home() {
     const argsForProducts = {
@@ -12,6 +13,7 @@ export default function Home() {
     };
     return (
         <Layout>
+            <MainSection />
             <Products {...argsForProducts} />
         </Layout>
     );
