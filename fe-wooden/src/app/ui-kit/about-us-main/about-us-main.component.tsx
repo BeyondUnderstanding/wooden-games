3138845f33738668a94f4eafcb832b655ab8fe8e
Gordeev Dmitry <jadima2015@gmail.com>
2023-11-03 20:48:00 +0300

    AboutUsMain
    Created a AboutUsMain component

---
+++ fe-wooden/src/app/ui-kit/about-us-main/about-us-main.component.tsx
@@ -0,0 +1,60 @@
+'use client';
+
+import css from './about-us-main.module.css';
+import Image from 'next/image';
+import ticTacToe from '../../../../public/img/short-information-section/ticTacToe.jpg';
+import balls from '../../../../public/img/short-information-section/chainBalls.jpg';
+
+export const AboutUsMain = () => {
+    return (
+        <div className={css.wrap}>
+            <h1 className={css.title}>About Us</h1>
+            <div className={css.content}>
+                <div className={css.element}>
+                    <div className={css.heading}>
+                        <h2 className={css.caption}>
+                            Roasting exquisite coffee while making an impact at
+                            origin and at home
+                        </h2>
+                        <p className={css.text}>
+                            The birthplace of wooden games is rightly considered
+                            to be England. Conservative yet progressive, it was
+                            England that gave the world the famous game
+                            of Jenga! The English love of this kind of game
+                            dates back to the kingdom’s distant past.2
+                        </p>
+                    </div>
+                    <Image
+                        width={820}
+                        height={580}
+                        src={ticTacToe}
+                        className={css.photo}
+                        alt="game photo"
+                    />
+                </div>
+                <div className={css.element}>
+                    <Image
+                        width={820}
+                        height={580}
+                        src={balls}
+                        className={css.photo}
+                        alt="game photo"
+                    />
+                    <div className={css.heading}>
+                        <h2 className={css.caption}>
+                            Roasting exquisite coffee while making an impact at
+                            origin and at home
+                        </h2>
+                        <p className={css.text}>
+                            The birthplace of wooden games is rightly considered
+                            to be England. Conservative yet progressive, it was
+                            England that gave the world the famous game
+                            of Jenga! The English love of this kind of game
+                            dates back to the kingdom’s distant past.2
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
