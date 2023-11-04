a701e32030a2f6b8cf02d36db2684f9806634d0d
Gordeev Dmitry <jadima2015@gmail.com>
2023-11-04 21:22:24 +0300

    FE-FE-4-FAQ-drop-down-component
    1. Created a drop-down icon
    2. Created a DropDownComponent

---
+++ fe-wooden/src/app/ui-kit/faq-drop-down/drop-down.component.tsx
@@ -0,0 +1,27 @@
+'use client';
+import React, { useState } from 'react';
+import css from './drop-down.module.css';
+import { DropDownIcon } from '../icons/drop-down-icon.component';
+
+export interface DropDownProps {
+    readonly qestion: string;
+    readonly answer: JSX.Element;
+}
+const DropDown = ({ qestion, answer }: DropDownProps) => {
+    const [isOpen, setOpen] = useState(false);
+
+
+    return (
+        <div className={css.wrap} onClick={() => setOpen(!isOpen)}>
+            <div className={css.titleWrap}>
+                <h1 className={css.title}>{qestion}</h1>
+                <DropDownIcon />
+            </div>
+            <div className= {`${css.answerWrap} ${isOpen?css.active:""}`}>
+                {answer}
+            </div>
+        </div>
+    );
+};
+
+export default DropDown;
