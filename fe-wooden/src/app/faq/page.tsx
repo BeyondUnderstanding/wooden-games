82458b53ac7b37519d7868b14b5506c9ff16d75b
Gordeev Dmitry <jadima2015@gmail.com>
2023-11-11 22:44:35 +0300

    faq-page

--- fe-wooden/src/app/faq/page.tsx
+++ fe-wooden/src/app/faq/page.tsx
@@ -1,5 +1,4 @@
 import { cookies } from 'next/headers';
-import { useUUID } from '../../utils/cookie.utils';
 import { restService } from '../service/global-action.service';
 import { Product } from '../ui-kit/side-popup/basket-popup.component';
 import { FaqPageContainer } from './faq-page.container';
@@ -7,7 +6,7 @@ import { FaqPageContainer } from './faq-page.container';
 const getProps = async () => {
     ('use server');
     const cookieStore = cookies();
-    let uuid = cookieStore.get('x-uuid');
+    const uuid = cookieStore.get('x-uuid');
 
     const service = restService();
 
@@ -25,7 +24,7 @@ const getProps = async () => {
 };
 
 export default async function Home() {
-    useUUID();
+
     const { productsResponce, basketResponce, occupiedDates } =
         await getProps();
     return (
